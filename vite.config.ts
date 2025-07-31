7ac6198081993a5d43c000f7548f3355395da3fc
Ashtogarh <107171068+Ashtogarh@users.noreply.github.com>
2025-07-31 15:43:03 +0530

    Update vite.config.ts
    EDIT

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/ReatApp/
 export default defineConfig({
   plugins: [react()],
 })
